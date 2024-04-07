21aaf0facc32f937f67aadec5691aab4773e4ed4
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2024-04-07 18:06:41 -0300

    ejercicio 1.1-3

--- src/guiaPOOUnidad1/Patente.java
+++ src/guiaPOOUnidad1/Patente.java
@@ -1,12 +1,24 @@
-
 package guiaPOOUnidad1;
-
 
 public class Patente {
 
-   
-    public static void main(String[] args) {
-       
+    String letrasInicioPatente;
+    int numeroPatente;
+    String letrasFinPatente;
+
+    public Patente() {
+        letrasInicioPatente = "";
+        numeroPatente = 0;
+        letrasFinPatente = "";
     }
-    
+
+    public Patente(String letrasInicioPatente, int numeroPatente, String letrasFinPatente) {
+        this.letrasInicioPatente = letrasInicioPatente;
+        this.numeroPatente = numeroPatente;
+        this.letrasFinPatente = letrasFinPatente;
+    }
+
+    public int getNumeroPatente() {
+        return this.numeroPatente;
+    }
 }
