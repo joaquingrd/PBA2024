06ba79da75818d1983dd16581d679d476408ba8c
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2024-04-07 18:35:15 -0300

    ejercicio 2

--- src/guiaPOOUnidad1/Patente.java
+++ src/guiaPOOUnidad1/Patente.java
@@ -7,9 +7,9 @@ public class Patente {
     String letrasFinPatente;
 
     public Patente() {
-        letrasInicioPatente = "";
-        numeroPatente = 0;
-        letrasFinPatente = "";
+        letrasInicioPatente = "AA";
+        numeroPatente = 111;
+        letrasFinPatente = "AA";
     }
 
     public Patente(String letrasInicioPatente, int numeroPatente, String letrasFinPatente) {
@@ -21,4 +21,33 @@ public class Patente {
     public int getNumeroPatente() {
         return this.numeroPatente;
     }
+
+    public void setNumeroPatente(int numeroPatente) {
+        if ((0 < numeroPatente) && (numeroPatente < 1000)) {
+        } else {
+            System.out.println("Número de Patente Inválido");
+        }
+
+    }
+
+    public String getLetrasInicioPatente() {
+        return letrasInicioPatente;
+    }
+
+    public String getLetrasFinPatente() {
+        return letrasFinPatente;
+    }
+
+    public void setLetrasInicioPatente(String letrasPatente) {
+        if (letrasPatente.length() < 3) {
+            this.letrasInicioPatente = letrasPatente;
+        } else {
+            System.out.println("Letras de Patente Inválida");
+        }
+    }
+
+    public void setLetrasFinPatente(String letrasFinPatente) {
+        this.letrasFinPatente = letrasFinPatente;
+    }
+
 }
