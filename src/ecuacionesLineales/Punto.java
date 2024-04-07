4cbaca71e0b971140471aa5bde9fe54b893ef907
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2024-04-07 19:22:48 -0300

    ejercicio 3

---
+++ src/ecuacionesLineales/Punto.java
@@ -0,0 +1,31 @@
+package ecuacionesLineales;
+
+public class Punto {
+
+    //abscisas 
+    private float x;
+    //ordenadas
+    private float y;
+
+    public Punto(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+}
