5fc9542d29b2951b29a89e3e5bbb67f40777802a
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2024-04-13 23:01:37 -0300

    Add Clase rectangulo

--- src/u2_practica_guiada/Rectangulo.java
+++ src/u2_practica_guiada/Rectangulo.java
@@ -1,6 +1,38 @@
 
 package u2_practica_guiada;
 
-public class Rectangulo {
+public class Rectangulo extends Figura{
     
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura, String nombre) {
+        super(nombre);
+        this.base = base;
+        this.altura = altura;
+    }
+    
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    @Override
+    protected double calcularArea() {
+        return this.base*this.altura;
+    }
+
 }
